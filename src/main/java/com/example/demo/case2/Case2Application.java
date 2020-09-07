1c5cd5d40cd6eba711fb4716565263ed56d51500
Hassan <apzhawk@gmail.com>
2020-09-07 14:13:39 +0800

    author

--- src/main/java/com/example/demo/case2/Case2Application.java
+++ src/main/java/com/example/demo/case2/Case2Application.java
@@ -3,6 +3,9 @@ package com.example.demo.case2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @Author Hassan
+ */
 @SpringBootApplication
 public class Case2Application {
 
